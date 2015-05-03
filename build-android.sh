6a73a7c555aff39b4b20a34bddba4749b3f00c68
Yale.Zheng <yale.zheng@icloud.com>
2015-05-03 16:43:54 +0800

    update build-android.sh

--- build-android.sh
+++ build-android.sh
@@ -2,9 +2,22 @@
 
 #设置ANDROID_NDK的根目录
 NDK_ROOT=/Users/ZhengYi/Public/android-ndk-r10
+
+#ffmpeg库的输出路径
 DEST_DIR=$(cd $(dirname $0); pwd)/android/arm
+
+#兼容arm-v7需要额外设置的Flag
 EXTRA_CFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
 EXTRA_LDFLAGS="-march=armv7-a -Wl,--fix-cortex-a8"
+
+#自定义工具链的配置信息，根据系统环境做相应调整
+#目标版本
+TARGET_PLATFORM=Android-L
+#目标工具链
+TARGET_TOOLCHAIN=arm-linux-androideabi-4.9
+#自定义工具链的输出路径
+TARGET_INSTALL_DIR=/tmp/android-toolchains/arm
+
 
 function main_entry {
     
@@ -19,16 +32,12 @@ function main_entry {
 
     #构建自定义工具链
     toolchain_builder=${NDK_ROOT}/build/tools/make-standalone-toolchain.sh
-    target_platform=Android-L
-    target_toolchain=arm-linux-androideabi-4.9
-    target_install_dir=/tmp/android-toolchains/arm
-    toolchain=$target_install_dir
     
     sh $toolchain_builder \
         --ndk-dir=$NDK_ROOT \
-        --platform=$target_platform \
-        --toolchain=$target_toolchain \
-        --install-dir=$target_install_dir
+        --platform=$TARGET_PLATFORM \
+        --toolchain=$TARGET_TOOLCHAIN \
+        --install-dir=$TARGET_INSTALL_DIR
     
     base_dir=$(dirname $0)
     cd $base_dir/src/ffmpeg
@@ -46,11 +55,11 @@ function main_entry {
         --disable-avdevice \
         --disable-doc \
         --disable-symver \
-        --cross-prefix=${toolchain}/bin/arm-linux-androideabi- \
+        --cross-prefix=${TARGET_INSTALL_DIR}/bin/arm-linux-androideabi- \
         --target-os=android \
         --arch=arm \
         --enable-cross-compile \
-        --sysroot=${toolchain}/sysroot \
+        --sysroot=${TARGET_INSTALL_DIR}/sysroot \
         --extra-cflags="$EXTRA_CFLAGS" \
         --extra-ldflags="$EXTRA_LDFLAGS"
     ##编译ffmpeg
