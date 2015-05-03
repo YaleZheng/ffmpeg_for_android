32f9abecf7e4e2438d307037a102b8f63b7aa972
Yale.Zheng <yale.zheng@icloud.com>
2015-05-03 16:35:34 +0800

    the build script

---
+++ build-android.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#设置ANDROID_NDK的根目录
+NDK_ROOT=/Users/ZhengYi/Public/android-ndk-r10
+DEST_DIR=$(cd $(dirname $0); pwd)/android/arm
+EXTRA_CFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
+EXTRA_LDFLAGS="-march=armv7-a -Wl,--fix-cortex-a8"
+
+function main_entry {
+    
+    #初始化
+    ffmpeg_dir=$(dirname $0)/src/ffmpeg
+    if [ -d $ffmpeg_dir ] ;then
+    echo "初始化完毕"
+    else
+    git clone git://source.ffmpeg.org/ffmpeg.git $ffmpeg_dir
+    echo "初始化完毕"
+    fi
+
+    #构建自定义工具链
+    toolchain_builder=${NDK_ROOT}/build/tools/make-standalone-toolchain.sh
+    target_platform=Android-L
+    target_toolchain=arm-linux-androideabi-4.9
+    target_install_dir=/tmp/android-toolchains/arm
+    toolchain=$target_install_dir
+    
+    sh $toolchain_builder \
+        --ndk-dir=$NDK_ROOT \
+        --platform=$target_platform \
+        --toolchain=$target_toolchain \
+        --install-dir=$target_install_dir
+    
+    base_dir=$(dirname $0)
+    cd $base_dir/src/ffmpeg
+    
+    ##配置ffmpeg
+    ./configure \
+        --prefix=$DEST_DIR \
+        --enable-shared \
+        --enable-static \
+        --disable-doc \
+        --disable-ffmpeg \
+        --disable-ffplay \
+        --disable-ffprobe \
+        --disable-ffserver \
+        --disable-avdevice \
+        --disable-doc \
+        --disable-symver \
+        --cross-prefix=${toolchain}/bin/arm-linux-androideabi- \
+        --target-os=android \
+        --arch=arm \
+        --enable-cross-compile \
+        --sysroot=${toolchain}/sysroot \
+        --extra-cflags="$EXTRA_CFLAGS" \
+        --extra-ldflags="$EXTRA_LDFLAGS"
+    ##编译ffmpeg
+    make -j4
+    make install
+    
+    cd -
+}
+
+#Script Logic
+if [ -d $NDK_ROOT ] ;then
+main_entry
+else
+script_name=`basename $0`
+echo "请配置 $script_name 中的变量NDK_ROOT"
+fi
+exit
